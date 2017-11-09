ac903eaa9a2ec2d4bebea1c9c9b7753069e98e13
ubbn <ubbn@live.com>
2017-11-09 11:47:22 +0100

    Added actions/reducers

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const store = createStore(
+  reducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
